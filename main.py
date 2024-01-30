97d91523bc915e3ebe666234ffbc4dc16d481f60
Gayanga Dulanjana <158056564+Gayangadw@users.noreply.github.com>
2024-01-30 10:28:50 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+# average of N numbers
+num = int(input("How many numbers are there? "))
+total_sum = 0
+
+for n in range(num):
+    numbers = float(input("What is the number? "))
+    total_sum += numbers
+
+average = total_sum / num
+print("The average is:", average)
+
+
+
+
