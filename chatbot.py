d0aab29ff7e87cb994e0feec7ff1bd128e862ccb
Gayanga Dulanjana <158056564+Gayangadw@users.noreply.github.com>
2024-03-09 22:30:51 +0530

    chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,48 @@
+# import numpy as np
+
+# class chatBot():
+#     def __init__(self, name):
+#         print("-----starting up", name, "-----")
+#         self.name = name
+
+# if __name__ == "__main__":
+#     ai = chatBot(name="dev")
+
+#     import speech_recognition as sr
+#     def speech_to_text(self):
+#         recognizer= sr.Recognizer()
+#         with sr.Microphone() as mic:
+#             print("Listening.............")
+#             audio = recognizer.listen(mic)
+#             try:
+#                 self.text = recognizer.recognize_google(audio)
+#                 print("me--->" , self.text)
+#             except:
+#                 print("me---> Error" )
+
+#                 if __name__== "__main__":
+#                     ai = chatBot(name="dev")
+#                     while True:
+import numpy as np
+import speech_recognition as sr
+
+class chatBot():
+    def __init__(self, name):
+        print("-----starting up", name, "-----")
+        self.name = name
+
+#     def speech_to_text(self):
+#         recognizer = sr.Recognizer()
+#         with sr.Microphone() as mic:
+#             print("Listening.............")
+#             audio = recognizer.listen(mic)
+#             try:
+#                 self.text = recognizer.recognize_google(audio)
+#                 print("me--->", self.text)
+#             except Exception as e:
+#                 print("me---> Error:", e)
+
+# if __name__ == "__main__":
+#     ai = chatBot(name="dev")
+#     while True:
+#         ai.speech_to_text()
